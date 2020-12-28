80a081add9df0b193dd5f89d74f717de3ac920a1
Kennan <kennan@gmail.com>
2020-12-27 19:04:11 -0500

    Updated responsive dropdown menu

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -5,12 +5,14 @@ import { AboutComponent } from "../about/about.component";
 import { SitesComponent } from "../sites/sites.component";
 import { ContactComponent } from "../contact/contact.component";
 import { PageNotFoundComponent } from "../page-not-found/page-not-found.component";
+import { HomeComponent } from "../home/home.component";
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent},
   { path: 'about', component: AboutComponent},
   { path: 'sites', component: SitesComponent},
   { path: 'contact', component: ContactComponent},
-  { path: '', redirectTo: '/about', pathMatch: 'full'},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent}
 ];
 
