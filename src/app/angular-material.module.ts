e465a732cfb6d302d5093ff6d69b6e5da6dfd143
Kennan <kennan@gmail.com>
2020-12-28 12:01:49 -0500

    Setup site card functionality

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -3,6 +3,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   //Imports are done automatically as an Angular included feature
@@ -10,7 +12,9 @@ import { MatIconModule } from '@angular/material/icon';
     MatToolbarModule,
     MatButtonModule,
     MatMenuModule,
-    MatIconModule
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule
   ]
 })
 export class AngularMaterialModule {}
--- src/app/sites/sites.component.ts
+++ src/app/sites/sites.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
 
+export interface Site_Cards {
+  title: string;
+  subTitle: string;
+  imageSrc: string;
+  altText: string;
+  paragraphText: string;
+  btnOneText: string;
+  btnTwoText: string;
+}
+
 @Component ({
   selector: 'sites',
   templateUrl: './sites.component.html',
   styleUrls: ['./sites.component.css']
 })
-export class SitesComponent {}
+export class SitesComponent {
+  siteCards: Site_Cards[] = [
+    {
+      title: 'Shiba Inu',
+      subTitle: 'Dog Breed',
+      imageSrc: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      altText: 'Photo of a Shiba Inu',
+      paragraphText: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
+      A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally
+      bred for hunting.`,
+      btnOneText: 'LIKE',
+      btnTwoText: 'SHARE'
+    }
+  ];
+}
