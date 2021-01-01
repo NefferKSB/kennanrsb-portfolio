b97c0994aac3cb0952bcfbb0220964f3206a879e
Kennan <kennan@gmail.com>
2021-01-01 15:25:27 -0500

    Fixed site cards stretch issue. There may still be a stretch issue with the contained image, but it is not very noticable.

--- src/app/sites/sites.component.ts
+++ src/app/sites/sites.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 export interface Site_Cards {
   title: string;
   imageSrc: string;
+  cols: number;
+  rows: any;
 }
 
 @Component ({
@@ -10,69 +12,78 @@ export interface Site_Cards {
   templateUrl: './sites.component.html',
   styleUrls: ['./sites.component.css']
 })
+
 export class SitesComponent implements OnInit {
+
+  /***********************************************
+  Logic for handling screen responsivness
+  The "colNum" and "margin" variable values
+  Are applied to the "mat-grid-list" html element
+  ***********************************************/
   public colNum: number;
-  public rowHeight: string;
   public margin: string;
+  public screenWidth = window.innerWidth;
 
   constructor() {
     this.colNum = 0;
-    this.rowHeight = '';
     this.margin ='';
   }
 
   ngOnInit() {
-    const screenWidth = window.innerWidth;
-
-    if(screenWidth > 700) {
-      this.colNum = 3;
-      this.rowHeight = "20em";
-      this.margin = "0 5em";
-    }
-    if(screenWidth <= 700) {
-      this.colNum = 2;
-      this.rowHeight = "20em";
-      this.margin = "0 1em";
-    }
-    if(screenWidth <= 400) {
-      this.colNum = 1;
-      this.rowHeight = "20em";
-      this.margin = "0 1em";
-    }
+    this.screenWidth = window.innerWidth;
+    this.setResponsiveAttrs(this.screenWidth);
   }
 
   onResize(event: any) {
-    const screenWidth = event.target.innerWidth;
-
-    if(screenWidth > 700) {
-      this.colNum = 3;
-      this.rowHeight = "20em";
-      this.margin = "0 5em";
-    }
-    if(screenWidth <= 700) {
-      this.colNum = 2;
-      this.rowHeight = "20em";
-      this.margin = "0 1em";
-    }
-    if(screenWidth <= 400) {
-      this.colNum = 1;
-      this.rowHeight = "20em";
-      this.margin = "0 1em";
-    }
+    this.screenWidth = event.target.innerWidth;
+    this.setResponsiveAttrs(this.screenWidth);
+    this.siteCards.forEach(card => card.rows = this.setSiteCardCal(this.screenWidth));
   }
+  /*********************************
+  End of screen responsiveness logic
+  *********************************/
 
   siteCards: Site_Cards[] = [
     {
       title: 'Site One',
-      imageSrc: 'https://material.angular.io/assets/img/examples/shiba2.jpg'
+      imageSrc: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      cols: 2,
+      rows: this.setSiteCardCal(this.screenWidth)
     },
     {
       title: 'Site Two',
-      imageSrc: 'https://material.angular.io/assets/img/examples/shiba2.jpg'
+      imageSrc: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      cols: 2,
+      rows: this.setSiteCardCal(this.screenWidth)
     },
     {
       title: 'Site Three',
-      imageSrc: 'https://material.angular.io/assets/img/examples/shiba2.jpg'
+      imageSrc: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      cols: 2,
+      rows: this.setSiteCardCal(this.screenWidth)
     }
   ];
+
+  setResponsiveAttrs(screenWidth: number) {
+    if(screenWidth > 700) {
+      this.colNum = 6;
+      this.margin = "0 5em";
+    }
+    if(screenWidth <= 700) {
+      this.colNum = 4;
+      this.margin = "0 1em";
+    }
+    if(screenWidth <= 400) {
+      this.colNum = 2;
+      this.margin = "0 1em";
+    }
+  }
+
+  setSiteCardCal(screenWidth: number) {
+    if(screenWidth <= 400){
+      return 6;
+    }
+    else
+      return 8;
+  }
 }
