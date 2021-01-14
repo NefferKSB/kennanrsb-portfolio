11a5969ffaaf043e8a19a3b39311e5aad3a551ed
NefferKSB <kennanrsb@gmail.com>
2021-01-14 13:31:05 -0500

    Added components experience, education, and skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,35 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { LayoutModule } from '@angular/cdk/layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './header/navbar.component';
 import { AngularMaterialModule } from './angular-material.module';
 import { RoutesModule } from './routes/routes.module';
+import { MaterialElevationDirective } from './material-elevation.directive';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './header/navbar.component';
+import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
-import { MaterialElevationDirective } from './material-elevation.directive';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    HomeComponent,
     AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
     ProjectsComponent,
     ContactComponent,
+    FooterComponent,
     MaterialElevationDirective
   ],
   imports: [
