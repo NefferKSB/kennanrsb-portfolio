566103c4448afa64ec2e4911d53720f9e9312317
NefferKSB <kennanrsb@gmail.com>
2021-01-23 15:37:35 -0500

    Updating Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kennanrsb-portfolio';
+
+  ngOnInit() {
+    AOS.init({
+      duration: 1200,
+      delay: 200,
+      once: true
+    });
+  }
 }
