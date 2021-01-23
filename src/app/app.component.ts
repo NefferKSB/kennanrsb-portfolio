5b5dc58bc7ec477626291d5fc2c5041c93e00473
NefferKSB <kennanrsb@gmail.com>
2021-01-23 15:41:57 -0500

    Angular upgrade 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import * as AOS from 'aos';
-import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'kennanrsb-portfolio';
-
-  ngOnInit() {
-    AOS.init({
-      duration: 1200,
-      delay: 200,
-      once: true
-    });
-  }
 }
