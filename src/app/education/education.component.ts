85d5aaf4429c053723725ad5c3f0b364e2cb54dc
NefferKSB <kennanrsb@gmail.com>
2021-01-25 12:17:18 -0500

    Added Education entry rendering logic

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { Education_Entry } from '../models/education-entry-model';
 
 @Component ({
   selector: 'education',
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.css']
 })
-export class EducationComponent {}
+export class EducationComponent {
+  educationEntries: Education_Entry[];
+
+  constructor() {
+    this.educationEntries = [
+      {
+        title: 'Mechanical Engineering & Technology',
+        eduType: 'Bachelors of Science',
+        where: 'Virginia State University',
+        dateRange: '2006 - 2010',
+        additionalInfo: [
+          'Cumulative GPA: 3.2'
+        ]
+      }
+    ]
+  }
+}
---
+++ src/app/models/education-entry-model.ts
@@ -0,0 +1,7 @@
+export interface Education_Entry {
+  dateRange: string;
+  eduType: string;
+  title: string;
+  where: string;
+  additionalInfo: string[];
+}
