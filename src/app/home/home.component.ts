6cd9074c99d0d296a5981a03840702937b6c43fa
NefferKSB <kennanrsb@gmail.com>
2021-01-28 16:04:25 -0500

    Fixed right side gap issue

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,4 @@ import { Component, HostListener } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  isScrolled = false;
-
-  @HostListener('window:scroll')
-  scrollEvent() {
-    window.pageYOffset >= 116 ? (this.isScrolled = true) : (this.isScrolled = false);
-  }
-}
+export class HomeComponent {}
